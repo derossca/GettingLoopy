8212812f4246f9a85e6ffb70f69f4df335bd44de
derossca <derossca@mail.uc.edu>
2024-02-21 07:27:09 -0500

    updated comments

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,53 @@
+//importing scanner
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+        //declaring variables most not needed, just in case I could redo them
+        Scanner in = new Scanner(System.in);
+        int num = 0;
+        String trash = "";
+        boolean done = false;
+
+        //TASK 1
+
+        for(int r = 0; r < 31; r++) // for loop that increments 1 at a time up to 30
+        {
+            System.out.printf("%3d |", r); // will print 3 digits plus a | counting to 30
+        }
+
+        System.out.println("\n");
+
+        //TASK 2
+
+        for(int r = 30; r > -1; r--) // for loop that decrements by 1 counting down from 30
+        {
+            System.out.println("" + r);
+        }
+
+        System.out.println("\n");
+
+        //TASK 3
+
+        for(int r = 0; r < 19 ; r += 3) // for loop that increments by 3 up to 18 but less than 19
+        {
+            System.out.println("" + r);
+        }
+
+        System.out.println("\n");
+
+        //TASK 4
+
+        for(int r = 10; r > -1; r -= 2) // for loop that decrements by 2 from 10 to 0 but is greater than -1
+        {
+            System.out.println("" + r);
+        }
+
+        System.out.println("\n");
+
+        //TASK 5
+
+        
     }
 }
